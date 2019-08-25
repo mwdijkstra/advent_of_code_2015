dae1103ba6e2cbe6b1b3209c8312477cbc9c7be8
marcel <m.w.dijkstra@gmail.com>
2019-08-25 13:51:17 +0200

    still working on day five puzzle two

--- src/main/java/day05/PuzzleTwo.java
+++ src/main/java/day05/PuzzleTwo.java
@@ -6,33 +6,81 @@ import static genericFunctions.GenericFunctions.getTime;
 
 public class PuzzleTwo {
 
-    public static final boolean LOGGING = true;
+    public static final boolean LOGGING = false;
 
     public boolean isThisStringNice(String input) {
 
-        return containsPairTwice(input) && false;
+        return containsPairTwice(input) && containsOneLetterBetweenRepeat(input);
 
     }
 
     public boolean containsPairTwice(String input) {
 
+        // todo: dit moet beter kunnen
+
         char[] listOfChars = input.toCharArray();
 
-        ArrayList<String> listOfPairs = new ArrayList<>();
+        ArrayList<String> uniqueListOfPairs = new ArrayList<>();
 
         for (int i = 0; i < listOfChars.length - 1; i++) {
 
             String pair = "" + listOfChars[i] + listOfChars[i+1];
 
-            listOfPairs.add(pair);
+            if (!uniqueListOfPairs.contains(pair)) {
+
+                uniqueListOfPairs.add(pair);
+
+            } else if (listOfChars[i-1] == listOfChars[i] && listOfChars[i+1] == listOfChars[i])  {
+
+                // uitzondering, niets doen?
+
+            } else {
+
+                if (LOGGING) {
+
+                    System.out.println(getTime() + " - Log: " + input + " contains this pair twice " + pair);
+
+                }
+
+                return true;
+
+            }
 
         }
-
-        System.out.println(listOfPairs);
 
         if (LOGGING) {
 
             System.out.println(getTime() + " - Log: " + input + " contains no pair twice");
+
+        }
+
+        return false;
+
+    }
+
+    public boolean containsOneLetterBetweenRepeat(String input) {
+
+        char[] listOfChars = input.toCharArray();
+
+        for (int i = 2; i < listOfChars.length; i++) {
+
+            if (listOfChars[i] == listOfChars[i-2]) {
+
+                if (LOGGING) {
+
+                    System.out.println(getTime() + " - Log: " + input + " contains this repeated letter with one letter in between " + listOfChars[i-2] + listOfChars[i-1] + listOfChars[i]);
+
+                }
+
+                return true;
+
+            }
+
+        }
+
+        if (LOGGING) {
+
+            System.out.println(getTime() + " - Log: " + input + " contains no repeated letter with one letter in between");
 
         }
 
--- src/test/java/day05/PuzzleTwoTest.java
+++ src/test/java/day05/PuzzleTwoTest.java
@@ -1,6 +1,9 @@
 package day05;
 
+import genericFunctions.GenericFunctions;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,6 +18,7 @@ public class PuzzleTwoTest {
         assertEquals(true, puzzle.isThisStringNice("xxyxx"));
         assertEquals(false, puzzle.isThisStringNice("uurcxstgmygtbstg"));
         assertEquals(false, puzzle.isThisStringNice("ieodomkazucvgmuy"));
+        assertEquals(true, puzzle.isThisStringNice("rxexcbwhiywwwwnu"));
 
     }
 
@@ -29,23 +33,48 @@ public class PuzzleTwoTest {
 
     }
 
+    @Test
+    public void containsOneLetterBetweenRepeatTest() {
 
+        PuzzleTwo puzzle = new PuzzleTwo();
 
+        assertEquals(true, puzzle.containsOneLetterBetweenRepeat("xyx"));
+        assertEquals(true, puzzle.containsOneLetterBetweenRepeat("abcdefeghi"));
+        assertEquals(true, puzzle.containsOneLetterBetweenRepeat("aaa"));
+        assertEquals(false, puzzle.containsOneLetterBetweenRepeat("test"));
 
-
-
-
-
-
-
-
-
-
+    }
 
     @Test
     public void getPuzzleAnswer() {
 
-        System.out.println();
+        // todo - the answer is 51 but I still get 50
+
+        //get input
+        ArrayList<String> list = GenericFunctions.getInputFromFile("C:\\Users\\Marcel\\IdeaProjects\\adventofcode2015\\src\\main\\java\\day05\\objects\\input");
+
+        //do stuff
+        PuzzleTwo puzzle = new PuzzleTwo();
+
+        int count = 0;
+
+        for (String string : list) {
+
+            if (puzzle.isThisStringNice(string)) {
+
+                count++;
+
+                //System.out.println(string);
+
+            } else {
+
+                //System.out.println(string + " is naughty");
+
+            }
+
+        }
+
+        System.out.println(count);
 
     }
 
